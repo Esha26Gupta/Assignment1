66a5c60e28ed2aecf37f2f8e03feea44617952d7
Esha26Gupta <egupta@expediagroup.com>
2021-05-26 16:49:15 +0530

    Images in ListView

--- app/src/main/java/com/example/assignment1/SubActivity1.kt
+++ app/src/main/java/com/example/assignment1/SubActivity1.kt
@@ -13,7 +13,7 @@ import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
-import com.example.assignment1.MySingleton.Companion.getInstance
+//import com.example.assignment1.MySingleton.Companion.getInstance
 import com.squareup.picasso.Picasso
 import org.json.JSONObject
 import java.lang.reflect.Array
@@ -29,21 +29,11 @@ class SubActivity1 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub1)
 
-        /*
-        val url = intent.getStringArrayExtra(URL)
-        val arrayAdapter: ArrayAdapter<*>
-        val users = arrayOf("A", "B", "C", "D", "E")
-
-        var mListView = findViewById<ListView>(R.id.userlist)
-        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, users)
-        mListView.adapter = arrayAdapter
-         */
-
+        //val test = intent.getStringArrayExtra(URL)
+        val testurl = "https://hub.dummyapis.com/ImagesList?text=Test&noofimages=10&height=120&width=120"
 
         val queue = Volley.newRequestQueue(this@SubActivity1)
 
-        val testurl =
-            "https://hub.dummyapis.com/ImagesList?text=Test&noofimages=10&height=120&width=120"
 
         val jsonArrayRequest = JsonArrayRequest(Request.Method.GET, testurl, null,
             { response ->
@@ -54,17 +44,15 @@ class SubActivity1 : AppCompatActivity() {
 
                 val image_url = List(response.length()) {
                     response.getString(it)
-                    Picasso.get().load(response.getString(it)).into(imageView)
+                    //Picasso.get().load(response.getString(it)).into(imageView)
                 }
 
-                val listAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, image_url)
+                val listAdapter = NewAdapter(this, image_url)
                 imageList.adapter = listAdapter
             },
             {})
 
             queue.add(jsonArrayRequest)
-
-            // MySingleton.getInstance(this).addToRequestQueue(jsonArrayRequest)
     }
 }
 
