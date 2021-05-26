aee6bbb6b1a6a23836946624f79a394bef1c1012
Esha26Gupta <egupta@expediagroup.com>
2021-05-26 13:10:27 +0530

    ListView without image

---
+++ app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.assignment1
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun OpenSubActivity1(view : View){
+        val url = "https://hub.dummyapis.com/ImagesList?text=Test&noofimages=10&height=120&width=120"
+        val intent = Intent(this, SubActivity1::class.java)
+        intent.putExtra(SubActivity1.URL, url)
+        startActivity(intent)
+
+    }
+}
---
+++ app/src/main/java/com/example/assignment1/SubActivity1.kt
@@ -0,0 +1,80 @@
+package com.example.assignment1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import com.example.assignment1.MySingleton.Companion.getInstance
+import com.squareup.picasso.Picasso
+import org.json.JSONObject
+import java.lang.reflect.Array
+import java.util.Calendar.getInstance
+
+class SubActivity1 : AppCompatActivity() {
+
+    companion object {
+        const val URL = "URL"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub1)
+        /*
+        val url = intent.getStringArrayExtra(URL)
+        val arrayAdapter: ArrayAdapter<*>
+        val users = arrayOf("A", "B", "C", "D", "E")
+
+        var mListView = findViewById<ListView>(R.id.userlist)
+        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, users)
+        mListView.adapter = arrayAdapter
+         */
+
+
+        val queue = Volley.newRequestQueue(this@SubActivity1)
+
+        val testurl =
+            "https://hub.dummyapis.com/ImagesList?text=Test&noofimages=10&height=120&width=120"
+
+        val jsonArrayRequest = JsonArrayRequest(Request.Method.GET, testurl, null,
+            { response ->
+                //println("Response is $it")
+
+                val image_url = List(response.length()) {
+                    response.getString(it)
+                }
+
+                val imageList = findViewById<ListView>(R.id.userlist)
+                val listAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, image_url)
+                imageList.adapter = listAdapter
+
+
+                /*
+                val ArrayAdapter = ArrayAdapter<String>(this, R.layout.item,Imageurl)
+
+
+                val userlist = findViewById<ListView>(R.id.userlist)
+                userlist.setAdapter(ArrayAdapter)
+
+
+                Imageurl.forEach{
+                    Picasso.get().load(it)
+                }
+                */
+            },
+
+            {})
+        queue.add(jsonArrayRequest)
+
+        // MySingleton.getInstance(this).addToRequestQueue(jsonArrayRequest)
+    }
+}
+
+
